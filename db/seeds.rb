2352e187d72a6251a0f1beeef5a6db1e2658b15f
chiii8 <144002417+chiii8@users.noreply.github.com>
2024-02-06 06:04:34 +0000

    [Add]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: 'ordereats@amail.com',
+  email: 'order_eats@email.com',
   password: '202309'
 )
