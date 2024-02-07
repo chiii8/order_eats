3c9b1cb3d1ae085a459c02fd257c3ae2534512bf
chiii8 <144002417+chiii8@users.noreply.github.com>
2024-02-07 06:56:09 +0000

    [Add]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,17 +1,17 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def new
     @order = Order.new
   end
-  
+
   def confirm
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items.all
     @total = 0
     @order.fee = 300
   end
-  
+
   def create
     # 注文を作成
     @order = Order.new(order_params)
@@ -21,9 +21,9 @@ class Public::OrdersController < ApplicationController
     # 注文番号を設定
     number = Order.maximum(:number)
     if number.nil?
-      @order.number = "M1"
+      @order.number = 1
     else
-      @order.number = "M#{number.to_i + 1}"
+      @order.number = "#{number.to_s}" + 1
     end
     @order.save
     #注文詳細を作成
@@ -36,24 +36,24 @@ class Public::OrdersController < ApplicationController
       @order_details.number = @order.number
       @order_details.save
     end
-    
+
     current_customer.cart_items.destroy_all
     redirect_to possible_path(order_id: @order.id)
   end
-  
+
   def index
     @orders = current_customer.orders.all
   end
-  
+
   def possible
     @order = Order.find(params[:order_id])
   end
-  
+
   def thanks
   end
-  
+
   private
-  
+
   def order_params
     params.require(:order).permit(:fee, :customer_id, :payment_method, :payment_amount)
   end
